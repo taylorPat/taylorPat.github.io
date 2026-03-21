2803df88dc557e1e6c9afbe348ccc4c2132f5018
taylorPat <patrick.schneider9@web.de>
2026-03-21 19:41:19 +0100

    feat: install ruff as dev dependency

--- portfolio/build.py
+++ portfolio/build.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import shutil
 
@@ -8,9 +7,10 @@ import yaml
 BASE_PATH = Path(__file__).resolve().parent
 PROFILE_FILE = BASE_PATH / "data" / "profile.yml"
 
-TEMPLATES_DIR=BASE_PATH / "templates"
+TEMPLATES_DIR = BASE_PATH / "templates"
 OUTPUT_DIR = BASE_PATH.parent / "docs"
 OUTPUT_FILE = OUTPUT_DIR / "index.html"
+
 
 def load_data(file_path: Path) -> yaml.YAMLObject:
     print(f"Loading YAML from: {file_path}")
@@ -22,17 +22,22 @@ def load_data(file_path: Path) -> yaml.YAMLObject:
         print("RAW CONTENT:")
         print(content)
         return yaml.safe_load(content)
-    
+
+
 def render_template(yaml_data, templates_dir, template_name: str):
-    env = Environment(loader=FileSystemLoader(searchpath=str(templates_dir)), autoescape=select_autoescape(["html", "xml"]))
+    env = Environment(
+        loader=FileSystemLoader(searchpath=str(templates_dir)),
+        autoescape=select_autoescape(["html", "xml"]),
+    )
     template = env.get_template(name=template_name)
     return template.render(yaml_data)
+
 
 def write_output(html: str, save_path: Path):
     save_path.parent.mkdir(parents=True, exist_ok=True)
     with save_path.open("w", encoding="utf-8") as file:
         file.write(html)
-    
+
 
 def copy_static():
     static_src = BASE_PATH / "static"
@@ -41,9 +46,11 @@ def copy_static():
         shutil.rmtree(static_dst)
     shutil.copytree(static_src, static_dst)
 
+
 def create_empty_docs_dir():
     shutil.rmtree(path=OUTPUT_DIR, ignore_errors=True)
     OUTPUT_DIR.mkdir(parents=True, exist_ok=False)
+
 
 def build():
     data = load_data(PROFILE_FILE)
